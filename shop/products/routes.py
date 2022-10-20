93ec0d0065067f68544b731ccc8bf5d45a5fcfed
Anton Fedorov <gtashnik2000@gmail.com>
2022-10-20 16:03:18 +0500

    part 16

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -2,8 +2,7 @@ from flask import redirect, session, render_template, url_for, flash, request, c
 from shop.__init__ import db, app, photos
 from shop.products.models import Brand, Category, Addproduct
 from shop.products.forms import Addproducts
-import secrets
-import os
+import secrets, os
 
 
 @app.route('/addproduct', methods=['GET', 'POST'])
@@ -61,25 +60,25 @@ def updateproduct(id):
         product.stock = form.stock.data
         product.color = form.color.data
         product.description = form.description.data
-    #
-    #     if request.files.get('image_1'):
-    #         try:
-    #             os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_1))
-    #             product.image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
-    #         except:
-    #             product.image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
-    #     if request.files.get('image_2'):
-    #         try:
-    #             os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_2))
-    #             product.image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
-    #         except:
-    #             product.image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
-    #     if request.files.get('image_3'):
-    #         try:
-    #             os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_3))
-    #             product.image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
-    #         except:
-    #             product.image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
+
+        if request.files.get('image_1'):
+            try:
+                os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_1))
+                product.image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
+            except:
+                product.image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
+        if request.files.get('image_2'):
+            try:
+                os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_2))
+                product.image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
+            except:
+                product.image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
+        if request.files.get('image_3'):
+            try:
+                os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_3))
+                product.image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
+            except:
+                product.image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
 
         flash('The product was updated', 'success')
         db.session.commit()
